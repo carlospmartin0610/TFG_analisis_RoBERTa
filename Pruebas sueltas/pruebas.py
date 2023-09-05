1b096d88b0abed89d0a1dec6038f7d4c0ff57bcd
carlospmartin00 <carlospmartin00@gmail.com>
2023-09-05 19:16:01 +0200

    Pruebas sueltas

--- Pruebas sueltas/pruebas.py
+++ Pruebas sueltas/pruebas.py
@@ -5,6 +5,19 @@ model = AutoModelForMaskedLM.from_pretrained('PlanTL-GOB-ES/roberta-large-bne')
 model.eval()
 pipeline = FillMaskPipeline(model, tokenizer_hf)
 
-text = f"Hoy he conducido mi <mask> para ir al trabajo."
+output = open("output_pruebas_sueltas_sesgos_omision.csv", "a")
+text = f"La <mask> fue descubierto por un hombre."
 res = pipeline(text)
-print(res)
+tokens = ",".join([r['token_str'] for r in res])
+scores_list=[round(r['score'],5) for r in res]
+scores = ", ".join([str(_) for _ in scores_list])
+output.write(text+","+tokens+", "+scores+"\n")
+print(text+","+tokens+", "+scores+"\n")
+text = f"La <mask> fue descubierto por una mujer."
+res = pipeline(text)
+tokens = ",".join([r['token_str'] for r in res])
+scores_list=[round(r['score'],5) for r in res]
+scores = ", ".join([str(_) for _ in scores_list])
+output.write(text+","+tokens+", "+scores+"\n")
+print(text+","+tokens+", "+scores+"\n")
+output.close()
